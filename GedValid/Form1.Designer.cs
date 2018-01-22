f522dc5ad92b76d6315ee6bcebaa2dea3025d6a4
fire-eggs <lifeattickville@gmail.com>
2018-01-22 12:47:24 -0500

    Initial commit

---
+++ GedValid/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace GedValid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openGEDCOMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recentFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printPreviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.printToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1016, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openGEDCOMToolStripMenuItem,
+            this.recentFilesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openGEDCOMToolStripMenuItem
+            // 
+            this.openGEDCOMToolStripMenuItem.Name = "openGEDCOMToolStripMenuItem";
+            this.openGEDCOMToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.openGEDCOMToolStripMenuItem.Text = "Open GEDCOM...";
+            this.openGEDCOMToolStripMenuItem.Click += new System.EventHandler(this.openGEDCOMToolStripMenuItem_Click);
+            // 
+            // recentFilesToolStripMenuItem
+            // 
+            this.recentFilesToolStripMenuItem.Name = "recentFilesToolStripMenuItem";
+            this.recentFilesToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.recentFilesToolStripMenuItem.Text = "Recent Files";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printPreviewToolStripMenuItem,
+            this.printSettingsToolStripMenuItem,
+            this.printToolStripMenuItem1});
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.printToolStripMenuItem.Text = "Print";
+            // 
+            // printPreviewToolStripMenuItem
+            // 
+            this.printPreviewToolStripMenuItem.Name = "printPreviewToolStripMenuItem";
+            this.printPreviewToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.printPreviewToolStripMenuItem.Text = "Print Preview";
+            this.printPreviewToolStripMenuItem.Click += new System.EventHandler(this.printPreviewToolStripMenuItem_Click);
+            // 
+            // printSettingsToolStripMenuItem
+            // 
+            this.printSettingsToolStripMenuItem.Name = "printSettingsToolStripMenuItem";
+            this.printSettingsToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.printSettingsToolStripMenuItem.Text = "Print Settings";
+            this.printSettingsToolStripMenuItem.Click += new System.EventHandler(this.printSettingsToolStripMenuItem_Click);
+            // 
+            // printToolStripMenuItem1
+            // 
+            this.printToolStripMenuItem1.Name = "printToolStripMenuItem1";
+            this.printToolStripMenuItem1.Size = new System.Drawing.Size(144, 22);
+            this.printToolStripMenuItem1.Text = "Print";
+            this.printToolStripMenuItem1.Click += new System.EventHandler(this.printToolStripMenuItem1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1016, 390);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 3);
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1010, 29);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(11, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(231, 329);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(265, 48);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(232, 329);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(511, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(502, 349);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(903, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 414);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openGEDCOMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recentFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printPreviewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GedValid/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GEDWrap;
+using SharpGEDParser;
+using SharpGEDParser.Model;
+
+using atup = System.Tuple<SharpGEDParser.Model.UnkRec.ErrorCode, string>;
+using atup2 = System.Tuple<SharpGEDParser.Model.UnkRec, SharpGEDParser.Model.GEDCommon>;
+
+
+namespace GedValid
+{
+    public partial class Form1 : Form
+    {
+        protected MruStripMenu mnuMRU;
+
+        public event EventHandler LoadGed;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            mnuMRU = new MruStripMenuInline(fileToolStripMenuItem, recentFilesToolStripMenuItem, OnMRU);
+            mnuMRU.MaxEntries = 7;
+            LoadGed += Form1_LoadGed;
+            LoadSettings(); // must go after mnuMRU init
+
+        }
+
+        private Dictionary<atup, List<atup2>> _errsByCodeAndTag;
+        private Dictionary<Issue.IssueCode, List<Issue>> _issByCode;
+        private Dictionary<string, List<atup2>> _unkByTag;
+
+        private class LBItem
+        {
+            public object data;
+            public string disp;
+            public int type;
+            public object key;
+            public override string ToString()
+            {
+                return disp;
+            }
+        }
+
+        public const int ERR = 1;
+        public const int ISS = 2;
+        public const int UNK = 3;
+
+        private void Form1_LoadGed(object sender, EventArgs e)
+        {
+            //logit("LoadGed 1", true);
+            Forest gedtrees = new Forest();
+            // TODO Using LastFile is a hack... pass path in args? not as event?            
+            gedtrees.LoadGEDCOM(LastFile);
+            //logit("LoadGed 2");
+
+            _errsByCodeAndTag = new Dictionary<atup, List<atup2>>();
+            _issByCode = new Dictionary<Issue.IssueCode, List<Issue>>();
+            _unkByTag = new Dictionary<string, List<atup2>>();
+
+            
+            // TODO can't get to reader top-level errors!
+            //gedtrees._gedReader.Errors;
+
+            foreach (var record in gedtrees.AllRecords)
+            {
+                foreach (var err in record.Errors)
+                {
+                    atup2 t2 = new atup2(err, record);
+
+                    atup t = new atup(err.Error, err.Tag);
+                    if (!_errsByCodeAndTag.ContainsKey(t))
+                        _errsByCodeAndTag.Add(t, new List<atup2> { t2 });
+                    else
+                    {
+                        _errsByCodeAndTag[t].Add(t2);
+                    }
+                }
+
+                foreach (var err in record.Unknowns)
+                {
+                    atup2 t2 = new atup2(err, record);
+
+                    if (!_unkByTag.ContainsKey(err.Tag))
+                        _unkByTag.Add(err.Tag, new List<atup2> { t2 });
+                    else
+                    {
+                        _unkByTag[err.Tag].Add(t2);
+                    }
+                }
+            }
+
+            // TODO need the issue<>gedcommon connection
+            foreach (var issue in gedtrees.Issues)
+            {
+                if (!_issByCode.ContainsKey(issue.IssueId))
+                    _issByCode.Add(issue.IssueId, new List<Issue>(){issue});
+                else
+                {
+                    _issByCode[issue.IssueId].Add(issue);
+                }
+            }
+
+            // list of errors
+            foreach (var key in _errsByCodeAndTag.Keys)
+            {
+                LBItem lbi = new LBItem();
+                lbi.type = ERR;
+                lbi.key = key;
+                lbi.data = _errsByCodeAndTag[key];
+                lbi.disp = string.Format(".Err:{0} Tag: {1} Count:{2}", key.Item1, key.Item2, _errsByCodeAndTag[key].Count);
+
+                listBox1.Items.Add(lbi);
+            }
+
+            // list of issues
+            foreach (var iss in _issByCode.Keys)
+            {
+                LBItem lbi = new LBItem();
+                lbi.type = ISS;
+                lbi.key = iss;
+                lbi.data = _issByCode[iss];
+                lbi.disp = string.Format(".Iss:{0} Count:{1}", iss, _issByCode[iss].Count);
+
+                listBox1.Items.Add(lbi);
+            }
+
+            // list of unknowns
+            foreach (var key in _unkByTag.Keys)
+            {
+                LBItem lbi = new LBItem();
+                lbi.key = key;
+                lbi.type = UNK;
+                lbi.data = _unkByTag[key];
+                lbi.disp = string.Format(".Unk:{0} Count:{1}", key, _unkByTag[key].Count);
+
+                listBox1.Items.Add(lbi);
+            }
+        }
+
+        private void OnMRU(int number, string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                mnuMRU.RemoveFile(number);
+                MessageBox.Show("The file no longer exists: " + filename);
+                return;
+            }
+
+            // TODO process could fail for some reason, in which case remove the file from the MRU list
+            LastFile = filename;
+            mnuMRU.SetFirstFile(number);
+            ProcessGED(filename);
+        }
+
+        private void openGEDCOMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = "GEDCOM files|*.ged;*.GED";
+            ofd.FilterIndex = 1;
+            ofd.DefaultExt = "ged";
+            ofd.CheckFileExists = true;
+            if (DialogResult.OK != ofd.ShowDialog(this))
+            {
+                return;
+            }
+            mnuMRU.AddFile(ofd.FileName);
+            LastFile = ofd.FileName; // TODO invalid ged file
+            ProcessGED(ofd.FileName);
+        }
+
+        private void ProcessGED(string gedPath)
+        {
+            Text = gedPath;
+            Application.DoEvents(); // Cycle events so image updates in case GED load/process takes a while
+            LoadGed(this, new EventArgs());
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        #region Settings
+        private DASettings _mysettings;
+
+        private List<string> _fileHistory = new List<string>();
+
+        private string LastFile
+        {
+            get
+            {
+                if (_fileHistory == null || _fileHistory.Count < 1)
+                    return null;
+                return _fileHistory[0]; // First entry is the most recent
+            }
+            set
+            {
+                // Make sure to wipe any older instance
+                _fileHistory.Remove(value);
+                _fileHistory.Insert(0, value); // First entry is the most recent
+            }
+        }
+
+        private void LoadSettings()
+        {
+            _mysettings = DASettings.Load();
+
+            // No existing settings. Use default.
+            if (_mysettings.Fake)
+            {
+                StartPosition = FormStartPosition.CenterScreen;
+            }
+            else
+            {
+                // restore windows position
+                StartPosition = FormStartPosition.Manual;
+                Top = _mysettings.WinTop;
+                Left = _mysettings.WinLeft;
+                Height = _mysettings.WinHigh;
+                Width = _mysettings.WinWide;
+                _fileHistory = _mysettings.PathHistory ?? new List<string>();
+                _fileHistory.Remove(null);
+                mnuMRU.SetFiles(_fileHistory.ToArray());
+
+                LastFile = _mysettings.LastPath;
+            }
+        }
+
+        private void SaveSettings()
+        {
+            // TODO check minimized
+            var bounds = DesktopBounds;
+            _mysettings.WinTop = Location.Y;
+            _mysettings.WinLeft = Location.X; 
+            _mysettings.WinHigh = bounds.Height;
+            _mysettings.WinWide = bounds.Width;
+            _mysettings.Fake = false;
+            _mysettings.LastPath = LastFile;
+            _mysettings.PathHistory = mnuMRU.GetFiles().ToList();
+            _mysettings.Save();
+        }
+        #endregion
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveSettings();
+        }
+
+        private void printPreviewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void printSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void printToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private class ListBoxItem2
+        {
+            public string txt;
+            public GEDCommon rec;
+            public UnkRec err;
+            public Issue iss;
+            public override string ToString()
+            {
+                return txt;
+            }
+        }
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            var obj = listBox1.SelectedItem as LBItem;
+
+            listBox2.Items.Clear(); // TODO delay update?
+
+            if (obj.type != ISS)
+            {
+                var foo = obj.data as List<atup2>;
+                atup2 first = foo[0];
+                if (obj.type == ERR)
+                    textBox1.Text = string.Format("Error: {0}, Tag:{2}, Count:{1}", first.Item1.Error, foo.Count, first.Item1.Tag);
+                else
+                    textBox1.Text = string.Format("Unknown: {0}, Count:{1}", first.Item1.Tag, foo.Count);
+
+                foreach (var tuple in foo)
+                {
+                    UnkRec unk = tuple.Item1;
+                    string val = string.Format("Lines:{0}-{1}", unk.Beg, unk.End);
+                    ListBoxItem2 lbi2 = new ListBoxItem2();
+                    lbi2.txt = val;
+                    lbi2.err = unk;
+                    lbi2.rec = tuple.Item2;
+                    listBox2.Items.Add(lbi2);
+                }
+            }
+            else
+            {
+                var foo = obj.data as List<Issue>;
+                Issue first = foo[0];
+                textBox1.Text = string.Format("Issue: {0}, Count:{1}", first.IssueId, foo.Count);
+                foreach (var iss in foo)
+                {
+                    string val = iss.Message();
+                    ListBoxItem2 lbi2 = new ListBoxItem2();
+                    lbi2.txt = val;
+                    lbi2.rec = null; // TODO need record link
+                    lbi2.iss = iss;
+                    listBox2.Items.Add(lbi2);
+                }
+            }
+        }
+
+        // NOTE: reading to record would be faster if during the parse, the relative position of each record was remembered
+        // NOTE: using record position may be required to cope with line break behavior
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBoxItem2 lbi = listBox2.SelectedItem as ListBoxItem2;
+            int beg = lbi.rec.BegLine;
+            int end = lbi.rec.EndLine;
+
+            var lins = ReadLineSet(beg, end);
+
+            int errbeg = lbi.err.Beg;
+            int errend = lbi.err.End;
+
+            richTextBox1.ResetText();
+            for (int i = beg; i <= end; i++)
+            {
+                string lin = lins[i - beg];
+                if (i >= errbeg && i <= errend)
+                    richTextBox1.SelectionColor = Color.Red;
+                else
+                    richTextBox1.SelectionColor = Color.Black;
+
+                richTextBox1.AppendText(lin);
+                richTextBox1.AppendText(Environment.NewLine);
+            }
+            // 1. have a record
+            // 2. get lines from file for the record
+            // 3. determine range of lines for the error
+            // 4. for each line from file:
+            // 4a. if line is within error range
+            // 4ai. set format to Red
+            // 4aii. else
+            // 4aiii. set format to black
+            // 4b. append text
+            // 4c. append environment.newline
+        }
+
+
+        private List<string> ReadLineSet(int beg, int end)
+        {
+            List<string> outl = new List<string>();
+
+            using (Stream stream = File.Open(LastFile, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    for (int i = 1; i < beg; i++)
+                        sr.ReadLine();
+                    for (int i = beg; i <= end; i++)
+                        outl.Add(sr.ReadLine());
+                }
+            }
+            return outl;
+        }
+    }
+}
