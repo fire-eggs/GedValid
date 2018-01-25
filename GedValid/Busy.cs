ac7b7a8cc52d1df5f996d70bafd20f8cbe33e7ef
fire-eggs <lifeattickville@gmail.com>
2018-01-25 13:10:03 -0500

    busy box

---
+++ GedValid/Busy.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace GedValid
+{
+    public partial class Busy : Form
+    {
+        public Busy()
+        {
+            InitializeComponent();
+
+            pictureBox1.Image = Properties.Resources.helix;
+        }
+
+        public string Msg
+        {
+            set { label1.Text = value; }
+        }
+    }
+}
